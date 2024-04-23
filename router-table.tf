2718d1de02c37afd08bea699aaf8c0c2ed9b464e
nagapoojithachirukuri <115095056+nagapoojithachirukuri@users.noreply.github.com>
2024-04-23 11:23:33 -0400

    Update router-table.tf

--- router-table.tf
+++ router-table.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table" "jhooq_public_route_table" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc-eu-central-1.id
   route {
     cidr_block = "0.0.0.0/0"
